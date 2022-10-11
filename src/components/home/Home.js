9a470ecf671967ad6d16e03df284a24bbd8af338
Lucas Comamala <lucascomamala@gmail.com>
2022-10-11 12:55:40 -0600

    Improve grid

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,17 +3,19 @@ import { useSelector } from 'react-redux';
 import Champion from './Champion';
 import './Home.css';
 
-const Home = () => {
+const HomePage = () => {
   const state = useSelector((state) => state.main);
   return (
     <div className="home-container">
-      <div className="champion-grid">
-        {state.simpleChampList.map((champ) => (
-          <Champion key={champ.id} champ={champ} />
-        ))}
+      <div className="champion-section">
+        <div className="champion-grid">
+          {state.simpleChampList.length ? state.simpleChampList.map((champ) => (
+            <Champion key={champ.id} champ={champ} />
+          )) : <div className="loading">Loading...</div>}
+        </div>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
